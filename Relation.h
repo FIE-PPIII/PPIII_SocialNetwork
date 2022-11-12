7394085fa80808e047b47d78789743d03e068e59
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-12 20:47:07 -0300

    ADD - enum Relation

---
+++ Relation.h
@@ -0,0 +1,13 @@
+//
+// Created by Usuario on 11/12/2022.
+//
+
+#ifndef BASE_CPP_RELATION_H
+#define BASE_CPP_RELATION_H
+
+enum Relation {
+    FRIEND,
+    BEST_FRIEND
+};
+
+#endif //BASE_CPP_RELATION_H
--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "Graph.h"
+#include "Relation.h"
 
 using namespace std;
 
 int main()
 {
     cout<<"Hello, world!"<<endl;
-    Graph<int, float, bool> network;
+    Graph<int, float, Relation> network;
     network.addNode(1, 1.0);
     network.addNode(2, 2.0);
     network.addNode(3, 3.0);
     network.addNode(4, 4.0);
 
-    network.setEdgeBetween(3, 4, true);
+    network.setEdgeBetween(3, 4, FRIEND);
     network.eraseNode(3);
 
     return 0;
